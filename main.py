504f458c94251f0d09bf682924b1be9b7fc9e1ea
Ashley Midgley <AshleyM@intergen.org.nz>
2018-09-07 16:14:19 +1200

    Initial commit

---
+++ main.py
@@ -0,0 +1,34 @@
+from flask import Flask, request, render_template
+from flask_bootstrap import Bootstrap
+import requests, bs4
+
+app = Flask(__name__)
+Bootstrap(app)
+
+class Bestseller:
+    def __init__(self, title, author, price ):
+        self.title = title
+        self.price = price
+        self.author = author
+
+@app.route('/')
+def index():
+    bestsellers = getBestsellers()
+    return render_template("index.html", bestsellers=bestsellers)
+
+def getBestsellers():
+    res = requests.get('https://www.bookdepository.com/')
+    soup = bs4.BeautifulSoup(res.text, 'lxml')
+    bestsellers = []
+    for i in soup.select('.tab-373 > .tab > .book-item > .item-info'):
+        title = i.select('.title > a')[0].text.replace(",", "").strip()
+        author = i.select('.author > span > a > span')[0].text.strip()
+        price = 'Unavailable'
+        if(i.select('.price-wrap > .price')):
+            price = i.select('.price-wrap > .price')[0].text.strip().split(' ')[0]
+        bs = Bestseller(title, author, price)
+        bestsellers.append(bs)
+    return bestsellers
+
+if __name__ == "__main__":
+    app.run(debug=True)
